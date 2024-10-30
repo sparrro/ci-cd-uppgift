82b372dcb7675c50b61c53b0ab5bfcc4620700cd
sparrro <116090214+sparrro@users.noreply.github.com>
2024-10-31 00:46:21 +0100

    debugging

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,4 +1,4 @@
-const { sendError } = require("../responses/index");
+const { sendError, sendResponse } = require("../responses/index");
 const { validateToken } = require("../utils/jwt");
 
 exports.authenticate = {
@@ -6,6 +6,7 @@ exports.authenticate = {
         
         const authorization = handler.event.headers["authorization"];
         const token = authorization && authorization.split(" ")[1];
+        return sendResponse(200, {token: token, authorization: authorization})
         if (!token) return sendError(401, "No valid token provided");
 
         const validated = validateToken(token);
