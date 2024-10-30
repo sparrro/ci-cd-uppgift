e09452103b074cfc96a1f24a7585b7d5aa035b4c
sparrro <116090214+sparrro@users.noreply.github.com>
2024-10-30 22:34:35 +0100

    get account

--- backend/functions/getAccount/index.js
+++ backend/functions/getAccount/index.js
@@ -1,6 +1,30 @@
 const { db } = require("./../../database/index");
-const { sendResponse } = require("../../responses/index")
+const { sendResponse, sendError } = require("../../responses/index");
+const { authenticate } = require("../../middlewares");
+const { QueryCommand } = require("@aws-sdk/lib-dynamodb");
 
-exports.handler = async (event) => {
-    
+getAccountHandler = async (event) => {
+
+    const id = event.user;
+
+    try {
+
+        const queryCommand = new QueryCommand({
+            TableName: "Users",
+            KeyConditionExpression: "id = :id",
+            ExpressionAttributeValues: {
+                ":id": id,
+            },
+        });
+        const result = await db.send(queryCommand);
+
+        return sendResponse(200, result);
+
+    } catch (error) {
+        console.error(error);
+        return sendError(500, "Server error");
+    }
+
 }
+
+exports.handler = middy(getAccountHandler).use(authenticate);
