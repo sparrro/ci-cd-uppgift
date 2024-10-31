904ed7c3584efe966965e48093fdc2a623a635c3
sparrro <116090214+sparrro@users.noreply.github.com>
2024-10-31 02:31:51 +0100

    get one meetup

--- backend/functions/getMeetup/index.js
+++ backend/functions/getMeetup/index.js
@@ -1,6 +1,27 @@
 const { db } = require("./../../database/index");
-const { sendResponse } = require("../../responses/index")
+const { sendResponse, sendError } = require("../../responses/index");
+const { QueryCommand } = require("@aws-sdk/lib-dynamodb");
 
 exports.handler = async (event) => {
+
+    const id = event.queryStringParameters["id"];
     
+    try {
+
+        const queryCommand = new QueryCommand({
+            TableName: "Meetups",
+            KeyConditionExpression: "id = :id",
+            ExpressionAttributeValues: {
+                ":id": id,
+            },
+        });
+        const result = await db.send(queryCommand);
+        if (result.Items.length==0) return sendError(404, "Not found");
+        return sendResponse(200, result.Items[0]);
+
+    } catch (error) {
+        console.error(error);
+        return sendError(500, "Server error");
+    }
+
 }
