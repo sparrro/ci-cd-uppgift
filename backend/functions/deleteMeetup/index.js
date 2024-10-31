4f3c4905e6b203312ab0b0b0f12b49a6b4e3219c
sparrro <116090214+sparrro@users.noreply.github.com>
2024-10-31 01:23:03 +0100

    delete meetup

--- backend/functions/deleteMeetup/index.js
+++ backend/functions/deleteMeetup/index.js
@@ -1,6 +1,27 @@
 const { db } = require("./../../database/index");
-const { sendResponse } = require("../../responses/index")
+const { sendResponse, sendError } = require("../../responses/index");
+const middy = require("@middy/core");
+const { authenticate, checkOwnership } = require("../../middlewares");
+const { DeleteCommand } = require("@aws-sdk/lib-dynamodb");
 
-exports.handler = async (event) => {
+deleteMeetupHandler = async (event) => {
     
+    const id = event.queryStringParameters["id"];
+
+    try {
+
+        const deleteCommand = new DeleteCommand({
+            TableName: "Meetups",
+            Key: {id: id}
+        });
+        await db.send(deleteCommand);
+        return sendResponse(200, "Idk what to put here lmao")
+
+    } catch (error) {
+        console.error(error);
+        return sendError(500, "Server error");
+    }
+
 }
+
+exports.handler = middy(deleteMeetupHandler).use(authenticate).use(checkOwnership);
--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,5 +1,7 @@
+const { QueryCommand } = require("@aws-sdk/lib-dynamodb");
 const { sendError, sendResponse } = require("../responses/index");
 const { validateToken } = require("../utils/jwt");
+const { db } = require("../database/index");
 
 exports.authenticate = {
     before: async (handler) => {
@@ -12,5 +14,21 @@ exports.authenticate = {
 
         handler.event.user = validated.user
 
+    }
+}
+
+exports.checkOwnership = {
+    before: async (handler) =>{
+        const meetupId = handler.event.queryStringParameters["id"];
+        const queryCommand = new QueryCommand({
+            TableName: "Meetups",
+            KeyConditionExpression: "id = :id",
+            ExpressionAttributeValues: {
+                ":id": meetupId,
+            },
+        });
+        const result = await db.send(queryCommand);
+        if (result.Items.length==0) return sendError(404, "No such meetup");
+        if (handler.event.user != result.Items[0].host) return sendError(403, "Can only delete own hosted meetups");
     }
 }
