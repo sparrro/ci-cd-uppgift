e9a79d9b396b9bbc6b67575c422ffecb5bf29b0c
Rolf Sparv <hrothi.wulfaz@gmail.com>
2024-10-31 21:55:27 +0100

    test

--- backend/responses/index.js
+++ backend/responses/index.js
@@ -17,7 +17,7 @@ exports.sendError = (status, message) => {
         statusCode: status,
         headers: {
             "Content-Type": "application/json",
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': 'http://127.0.0.1:5500',
             'Access-Control-Allow-Credentials': true,
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type",
